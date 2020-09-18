6a01663e879a2b3ba37dce563d99087c5ef42160
oddlama <oddlama@oddlama.org>
2020-09-19 01:19:57 +0200

    Prepare update and start scripts

---
+++ start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export PATH="/opt/oracle-jdk-bin-11.0.2/bin:$PATH"
+export JAVA_HOME="/opt/oracle-jdk-bin-11.0.2"
+
+java -Xms10G -Xmx10G \
+	-XX:+UseG1GC \
+	-XX:+ParallelRefProcEnabled \
+	-XX:MaxGCPauseMillis=200 \
+	-XX:+UnlockExperimentalVMOptions \
+	-XX:+DisableExplicitGC \
+	-XX:+AlwaysPreTouch \
+	-XX:G1NewSizePercent=30 \
+	-XX:G1MaxNewSizePercent=40 \
+	-XX:G1HeapRegionSize=8M \
+	-XX:G1ReservePercent=20 \
+	-XX:G1HeapWastePercent=5 \
+	-XX:G1MixedGCCountTarget=4 \
+	-XX:InitiatingHeapOccupancyPercent=15 \
+	-XX:G1MixedGCLiveThresholdPercent=90 \
+	-XX:G1RSetUpdatingPauseTimePercent=5 \
+	-XX:SurvivorRatio=32 \
+	-XX:+PerfDisableSharedMem \
+	-XX:MaxTenuringThreshold=1 \
+	-Dusing.aikars.flags=https://mcflags.emc.gs \
+	-Daikars.new.flags=true \
+	-jar paperclip.jar nogui
+
+# Backup world
+LOGFILE="logs/backup.log"
+BACKUP_TO="backup"
+BACKUP_DIRS=(
+	'plugins'
+	'world'
+	'world_nether'
+	'world_the_end'
+)
+
+mkdir -p "$BACKUP_TO" &>/dev/null
+
+for i in "${!BACKUP_DIRS[@]}"; do
+	echo "$(date) backing up ${BACKUP_DIRS[$i]}" &>> "$LOGFILE"
+	rdiff-backup "${BACKUP_DIRS[$i]}" "$BACKUP_TO/${BACKUP_DIRS[$i]}" &>> "$LOGFILE"
+done
+
+echo "$(date) backup finished" &>> "$LOGFILE"
---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -uo pipefail
+
+die() {
+	echo "[1;31merror:[m $*" >&2
+	exit 1
+}
+
+#waterfall_version="$(http GET "https://papermc.io/api/v1/waterfall" | jq -r ".versions[0]")" \
+#	|| die "Error while retrieving waterfall version"
+#waterfall_build="$(http GET "https://papermc.io/api/v1/waterfall/$waterfall_version" | jq -r ".builds.latest")" \
+#	|| die "Error while retrieving waterfall build"
+#
+#curl "https://papermc.io/api/v1/waterfall/$waterfall_version/$waterfall_build/download" \
+#	-o waterfall.jar
+
+# Download paper
+paper_version="$(http GET "https://papermc.io/api/v1/paper" | jq -r ".versions[0]")" \
+	|| die "Error while retrieving paper version"
+paper_build="$(http GET "https://papermc.io/api/v1/paper/$paper_version" | jq -r ".builds.latest")" \
+	|| die "Error while retrieving paper build"
+
+echo "[+] Downloading paper version $paper_version build $paper_build"
+curl "https://papermc.io/api/v1/paper/$paper_version/$paper_build/download" \
+	-o paper.jar
+
+# Download plugins
+# vane Clone repo, build?
+# ProtocolLib
+# Dynmap
+# WorldBorder
+# WorldEdit?
