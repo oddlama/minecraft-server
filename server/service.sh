7e13cc9848ecc6881cc94cb42c47ecc9a2e1ec0f
oddlama <oddlama@oddlama.org>
2020-09-19 15:34:49 +0200

    Prepare for service.sh

---
+++ server/service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -uo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null \
+	|| exit 1
+source "../env.sh" || exit 1
+
+case "$1" in
+	"start")
+		"./server.sh" &
+		;;
+
+	"stop")   ;;
+	"attach") ;;
+esac
